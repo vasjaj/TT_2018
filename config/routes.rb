d8a316ba697599a50430a75cd71091f7074e6bfd
vasilijs.jelcaninovs <vasilijs.jelcaninovs@mogofinance.com>
2018-06-15 22:43:55 +0300

    added admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :items
+
+      root to: "users#index"
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   devise_for :users

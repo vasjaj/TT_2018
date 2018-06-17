1c33ea46426d7b045a431502a6121aa5b625d13a
vasilijs.jelcaninovs <vasilijs.jelcaninovs@mogofinance.com>
2018-06-17 20:29:56 +0300

    added view for admin log

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
       resources :users
       resources :items
-
+      resources :logs
       root to: "users#index"
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

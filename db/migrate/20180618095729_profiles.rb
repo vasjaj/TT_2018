5c6ea21f90df3c8db407b760b31f0b79d9ef8ca7
SmallUnicorn <alexj10903@gmail.com>
2018-06-18 13:39:41 +0300

    fix

---
+++ db/migrate/20180618095729_profiles.rb
@@ -0,0 +1,4 @@
+class Profiles < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_012422) do
+ActiveRecord::Schema.define(version: 2018_06_18_095729) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
